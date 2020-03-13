88132117c57fda0314a903b72fb6bf83378ff15c
Naarveil <57903192+Naarveil@users.noreply.github.com>
2020-03-13 12:19:08 +0100

    Push Synchronize + NetworkItem Scripts

---
+++ Assets/Scripts/Network/NM_NetworkItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NM_NetworkItem : NM_Synchronize
+{
+
+    protected virtual void Update()
+    {
+        if (myID.IsMine)
+            OnLocal();
+        else
+            OnOnline();
+    }
+
+    public override void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){}
+    protected override void OnLocal(){}
+    protected override void OnOnline(){}
+}
---
+++ Assets/Scripts/Network/NM_Synchronize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public abstract class NM_Synchronize : MonoBehaviour, IPunObservable
+{
+    [SerializeField] protected PhotonView myID;
+
+    protected abstract void OnLocal();
+    protected abstract void OnOnline();
+    public abstract void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info);
+}
