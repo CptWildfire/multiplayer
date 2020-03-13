3c7492e4f329f98c4711e8021a1505f70e386335
ADereusme <57903210+ADereusme@users.noreply.github.com>
2020-03-13 15:07:29 +0100

    Test sync color

--- Assets/Scripts/Network/NM_PlayerNetwork.cs
+++ Assets/Scripts/Network/NM_PlayerNetwork.cs
@@ -12,6 +12,8 @@ public class NM_PlayerNetwork : MonoBehaviour, IPunObservable
     {
         //name = myID.ViewID.ToString();
         name = myID.Owner.NickName;
+        if (myID.IsMine) GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.red);
+        myID.RPC("SetColor", Photon.Pun.RpcTarget.AllBuffered, null);
     }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){}
 }
